beb7e1baee7bfa662ed817fa79c0628a098b9d18
AhmedHdeawy <ahmedhdeawy@gmail.com>
2025-04-22 19:55:48 +0200

    added telescope for local monitoring only

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,7 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\ServiceClassProvider::class,
     App\Providers\RepositoryServiceProvider::class,
     App\Providers\RouteServiceProvider::class,
+    App\Providers\ServiceClassProvider::class,
 ];
