50f81fa11f8c6dd43481f001e390a993e62df591
enrico <enrico@LAPTOP-94IRVKU8.localdomain>
2025-09-23 12:28:04 +0700

    fix(welcome): update welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,8 +21,8 @@
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
         <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
-            <main>
-                <h1>GKOMUNIKA MAIN SERVICE</h1>
+            <main class="text-dark dark:text-white">
+                <h1 class="font-bold text-3xl mb-2">GKOMUNIKA MAIN SERVICE</h1>
                 <p>v1</p>
             </main>
         </div>
